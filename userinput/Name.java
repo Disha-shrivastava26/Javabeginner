e63bd5345392b7a358643129bcdfa180aeb0f1c7
Disha shrivastava <shrivastavadisha26@gmail.com>
2025-08-02 21:17:01 +0530

    Create Name.java

---
+++ userinput/Name.java
@@ -0,0 +1,14 @@
+package classpractise;
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String args[]) {
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("Enter Username");
+
+        String username = myobj.nextLine();
+        System.out.println("username is =" + username);
+
+    }
+
+}
