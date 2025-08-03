91472e81cdcfb21ac853431d86f83afb5f09a94b
Disha shrivastava <shrivastavadisha26@gmail.com>
2025-08-03 16:08:25 +0530

    Update pattern1.java

--- pattern/pattern1.java
+++ pattern/pattern1.java
@@ -32,7 +32,6 @@ class Pattern{
     }
 }
 
-//"C:\Program Files\Java\jdk-24\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2025.1.4.1\lib\idea_rt.jar=58111" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath D:\JavaCodes\out\production\JavaCodes classpractise.Pattern
 print the pattern
 enter the number of rows
 5
